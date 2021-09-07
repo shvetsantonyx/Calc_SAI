0f3b2256687cb5336e0d29b3cce8fe82f59272c7
shvetsantonyx <90096233+shvetsantonyx@users.noreply.github.com>
2021-09-08 01:09:41 +0300

    new class

--- Calc_2/Calc_SAI.py
+++ Calc_2/Calc_SAI.py
@@ -3,7 +3,7 @@ from tkinter import *
 import buttons as b
 
 
-class Calc_app(Tk, b.Buttonsaction):
+class Calc_app(Tk):
 
     def __init__(self):
 
@@ -13,7 +13,6 @@ class Calc_app(Tk, b.Buttonsaction):
         self.geometry('+900+300')
         self.resizable(False, False)
         # self.iconbitmap('calc_calculator_2183.ico')
-
         self.set_ui()
 
 
@@ -23,32 +22,36 @@ class Calc_app(Tk, b.Buttonsaction):
         self.entry.insert(0, '0')
         self.entry.grid(row=0, column=0, columnspan=4)
 
-        btn_del = Button(font=('Segoe UI', 14, 'bold'), text='Del', width=6, bg='#DCDCDC', command=lambda: b.Buttonsaction.btn_del_action(self)).grid(row=1, column=0)
-        btn_square = Button(font=('Segoe UI', 14, 'bold'), text='x²', width=6, bg='#DCDCDC', command=lambda: b.Buttonsaction.btn_square_action(self)).grid(row=1, column=1)
-        btn_sqrt = Button(font=('Segoe UI', 14, 'bold'), text='²√x', width=6, bg='#DCDCDC', command=lambda: b.Buttonsaction.btn_sqrt_action(self)).grid(row=1, column=2)
-        btn_division = Button(font=('Segoe UI', 14, 'bold'), text='÷', width=4, bg='#DCDCDC', command=lambda: b.Operations_main.btn_operation_action(self, 'division')).grid(row=1, column=3)
+        Buttons = b.Buttonsaction()
 
-        btn_7 = Button(font=('Segoe UI', 14, 'bold'), text='7', width=6, bg='white', command=lambda: b.Buttonsaction.btn_num_action(self, 7)).grid(row=2, column=0)
-        btn_8 = Button(font=('Segoe UI', 14, 'bold'), text='8', width=6, bg='white', command=lambda: b.Buttonsaction.btn_num_action(self, 8)).grid(row=2, column=1)
-        btn_9 = Button(font=('Segoe UI', 14, 'bold'), text='9', width=6, bg='white', command=lambda: b.Buttonsaction.btn_num_action(self, 9)).grid(row=2, column=2)
-        btn_x = Button(font=('Segoe UI', 14, 'bold'), text='x', width=4, bg='#DCDCDC', command=lambda: b.Operations_main.btn_operation_action(self, 'multiply')).grid(row=2, column=3)
 
-        btn_4 = Button(font=('Segoe UI', 14, 'bold'), text='4', width=6, bg='white', command=lambda: b.Buttonsaction.btn_num_action(self, 4)).grid(row=3, column=0)
-        btn_5 = Button(font=('Segoe UI', 14, 'bold'), text='5', width=6, bg='white', command=lambda: b.Buttonsaction.btn_num_action(self, 5)).grid(row=3, column=1)
-        btn_6 = Button(font=('Segoe UI', 14, 'bold'), text='6', width=6, bg='white', command=lambda: b.Buttonsaction.btn_num_action(self, 6)).grid(row=3, column=2)
-        btn_minus = Button(font=('Segoe UI', 14, 'bold'), text='-', width=4, bg='#DCDCDC', command=lambda: b.Operations_main.btn_operation_action(self, 'minus')).grid(row=3, column=3)
 
-        btn_1 = Button(font=('Segoe UI', 14, 'bold'), text='1', width=6, bg='white', command=lambda: b.Buttonsaction.btn_num_action(self, 1)).grid(row=4, column=0)
-        btn_2 = Button(font=('Segoe UI', 14, 'bold'), text='2', width=6, bg='white', command=lambda: b.Buttonsaction.btn_num_action(self, 2)).grid(row=4, column=1)
-        btn_3 = Button(font=('Segoe UI', 14, 'bold'), text='3', width=6, bg='white', command=lambda: b.Buttonsaction.btn_num_action(self, 3)).grid(row=4, column=2)
-        btn_plus = Button(font=('Segoe UI', 14, 'bold'), text='+', width=4, bg='#DCDCDC', command=lambda: b.Operations_main.btn_operation_action(self, 'plus')).grid(row=4, column=3)    
+        btn_del = Button(font=('Segoe UI', 14, 'bold'), text='Del', width=6, bg='#DCDCDC', command=lambda: Buttons.btn_del_action(self)).grid(row=1, column=0)
+        btn_square = Button(font=('Segoe UI', 14, 'bold'), text='x²', width=6, bg='#DCDCDC', command=lambda: Buttons.btn_square_action(self)).grid(row=1, column=1)
+        btn_sqrt = Button(font=('Segoe UI', 14, 'bold'), text='²√x', width=6, bg='#DCDCDC', command=lambda: Buttons.btn_sqrt_action(self)).grid(row=1, column=2)
+        btn_division = Button(font=('Segoe UI', 14, 'bold'), text='÷', width=4, bg='#DCDCDC', command=lambda: Buttons.btn_operation_action(self, 'division')).grid(row=1, column=3)
+
+        btn_7 = Button(font=('Segoe UI', 14, 'bold'), text='7', width=6, bg='white', command=lambda: Buttons.btn_num_action(self, 7)).grid(row=2, column=0)
+        btn_8 = Button(font=('Segoe UI', 14, 'bold'), text='8', width=6, bg='white', command=lambda: Buttons.btn_num_action(self, 8)).grid(row=2, column=1)
+        btn_9 = Button(font=('Segoe UI', 14, 'bold'), text='9', width=6, bg='white', command=lambda: Buttons.btn_num_action(self, 9)).grid(row=2, column=2)
+        btn_x = Button(font=('Segoe UI', 14, 'bold'), text='x', width=4, bg='#DCDCDC', command=lambda: Buttons.btn_operation_action(self, 'multiply')).grid(row=2, column=3)
+
+        btn_4 = Button(font=('Segoe UI', 14, 'bold'), text='4', width=6, bg='white', command=lambda: Buttons.btn_num_action(self, 4)).grid(row=3, column=0)
+        btn_5 = Button(font=('Segoe UI', 14, 'bold'), text='5', width=6, bg='white', command=lambda: Buttons.btn_num_action(self, 5)).grid(row=3, column=1)
+        btn_6 = Button(font=('Segoe UI', 14, 'bold'), text='6', width=6, bg='white', command=lambda: Buttons.btn_num_action(self, 6)).grid(row=3, column=2)
+        btn_minus = Button(font=('Segoe UI', 14, 'bold'), text='-', width=4, bg='#DCDCDC', command=lambda: Buttons.btn_operation_action(self, 'minus')).grid(row=3, column=3)
+
+        btn_1 = Button(font=('Segoe UI', 14, 'bold'), text='1', width=6, bg='white', command=lambda: Buttons.btn_num_action(self, 1)).grid(row=4, column=0)
+        btn_2 = Button(font=('Segoe UI', 14, 'bold'), text='2', width=6, bg='white', command=lambda: Buttons.btn_num_action(self, 2)).grid(row=4, column=1)
+        btn_3 = Button(font=('Segoe UI', 14, 'bold'), text='3', width=6, bg='white', command=lambda: Buttons.btn_num_action(self, 3)).grid(row=4, column=2)
+        btn_plus = Button(font=('Segoe UI', 14, 'bold'), text='+', width=4, bg='#DCDCDC', command=lambda: Buttons.btn_operation_action(self, 'plus')).grid(row=4, column=3)    
         
-        btn_clear = Button(font=('Segoe UI', 14, 'bold'), text='C', width=6, bg='#DCDCDC', command=lambda: b.Operations_main.btn_clear_action(self)).grid(row=5, column=0)
-        btn_0 = Button(font=('Segoe UI', 14, 'bold'), text='0', width=6, bg='white', command=lambda: b.Buttonsaction.btn_num_action(self, 0)).grid(row=5, column=1)
-        btn_comma = Button(font=('Segoe UI', 14, 'bold'), text=',', width=6, bg='white', command=lambda: b.Operations_main.btn_comma_action(self)).grid(row=5, column=2)
-        btn_equal = Button(font=('Segoe UI', 14, 'bold'), text='=', width=4, bg='#DCDCDC', command=lambda: b.Equal_action.btn_equal_action(self)).grid(row=5, column=3)
+        btn_clear = Button(font=('Segoe UI', 14, 'bold'), text='C', width=6, bg='#DCDCDC', command=lambda: Buttons.btn_clear_action(self)).grid(row=5, column=0)
+        btn_0 = Button(font=('Segoe UI', 14, 'bold'), text='0', width=6, bg='white', command=lambda: Buttons.btn_num_action(self, 0)).grid(row=5, column=1)
+        btn_comma = Button(font=('Segoe UI', 14, 'bold'), text=',', width=6, bg='white', command=lambda: Buttons.btn_comma_action(self)).grid(row=5, column=2)
+        btn_equal = Button(font=('Segoe UI', 14, 'bold'), text='=', width=4, bg='#DCDCDC', command=lambda: Buttons.btn_equal_action(self)).grid(row=5, column=3)
 
-
+# https://stackoverflow.com/questions/2283210/python-function-pointer
 if __name__ == '__main__':
     root = Calc_app()
     root.mainloop()
--- Calc_2/buttons.py
+++ Calc_2/buttons.py
@@ -1,163 +1,211 @@
-from tkinter.constants import END
+from tkinter.constants import END, FALSE, NONE
 from math import sqrt
 
 
 class Buttonsaction:
 
-    # marker for operations: plus, minus, multyply, division
-    # global marker_set
-    # marker_set = None
+    def __init__(self):
 
-    # # marker for new turn of btn_num_action after operations
-    # global marker_set_2
-    # marker_set_2 = True
+        self.marker_set = None
+        self.marker_set_2 = False
+        self.counter_operations = 0
+        self.previous_res = 0
+        self.previous_operation = None
 
 
-    def btn_num_action(self, constant):
+    def btn_num_action(self, tkinter, constant):
 
-        BTN = constant
-
-        previous_num = self.entry.get()
-
+        previous_num = tkinter.entry.get()
 
         if previous_num == '0':
-            self.entry.delete(first=0, last=END)
-            self.entry.insert(0, BTN)
+            tkinter.entry.delete(first=0, last=END)
+            tkinter.entry.insert(0, constant)
         elif previous_num == '0' and '.' in previous_num:
-            self.entry.insert(len(previous_num), BTN)
+            tkinter.entry.insert(len(previous_num), constant)
         elif previous_num != '0' and '.' not in previous_num:
-            self.entry.insert(len(previous_num), BTN)
+            tkinter.entry.insert(len(previous_num), constant)
         elif previous_num != 0 and '.' in previous_num:
-            self.entry.insert(len(previous_num), BTN)
+            tkinter.entry.insert(len(previous_num), constant)
 
         
         if self.marker_set_2 == False:
-            self.entry.delete(first=0, last=END)
+            tkinter.entry.delete(first=0, last=END)
             self.marker_set_2 = True
-            self.btn_num_action(constant)
+            self.btn_num_action(tkinter, constant)
 
-        
+        self.previous_res = previous_num
 
 
-class Operations_main(Buttonsaction):
-
-    def btn_operation_action(self, marker):
-
-        # global marker_set
-        self.marker_set = marker
-
-        # global marker_set_2
-        self.marker_set_2 = False
-
-        # global previous_res
-        self.previous_res = self.entry.get()
+    def btn_operation_action(self, tkinter, marker):
 
         if self.marker_set == None:
-            self.entry.delete(first=0, last=END)
-            self.entry.insert(0, '0')
+            self.previous_operation = marker
+            self.marker_set = marker
+            self.marker_set_2 = False
+            self.counter_operations += 1
 
+        else:  
+            self.marker_set = marker
+            self.marker_set_2 = False
+            self.counter_operations += 1
 
-    def btn_square_action(self):
+        if self.counter_operations == 1:
+            self.previous_marker = self.marker_set
 
-        previous_num = float(self.entry.get())
-        self.entry.delete(first=0, last=END)
+        tkinter.previous_res = tkinter.entry.get()
+
+        if self.marker_set == None:
+            tkinter.entry.delete(first=0, last=END)
+            tkinter.entry.insert(0, '0')
+
+        if self.counter_operations > 1:
+            tkinter.previous_res = self.previous_res
+            self.equal_action(tkinter)
+            tkinter.previous_res = tkinter.entry.get()
+            
+
+    def btn_square_action(self, tkinter):
+
+        previous_num = float(tkinter.entry.get())
+        tkinter.entry.delete(first=0, last=END)
         result = previous_num ** 2
         if str(result)[-1] == '0':
-            self.entry.insert(0, str(result)[:-2])
+            tkinter.entry.insert(0, str(result)[:-2])
         else:
-            self.entry.insert(0, str(result))
+            tkinter.entry.insert(0, str(result))
 
 
-    def btn_comma_action(self):
+    def btn_comma_action(self, tkinter):
 
-        previous_num = self.entry.get()
+        previous_num = tkinter.entry.get()
 
         if previous_num != 0 and '.' not in previous_num:
-            self.entry.insert(len(previous_num), '.')
+            tkinter.entry.insert(len(previous_num), '.')
 
         elif previous_num !=0 and '.' in previous_num:
             pass
 
         if previous_num == 0:
-            self.entry.insert(1, '.')
+            tkinter.entry.insert(1, '.')
 
 
-    def btn_del_action(self):
-        previous_num = self.entry.get()
+    def btn_del_action(self, tkinter):
+        previous_num = tkinter.entry.get()
 
         if previous_num != '0':
-            self.entry.delete(first=(len(previous_num) - 1), last=None)
+            tkinter.entry.delete(first=(len(previous_num) - 1), last=None)
 
-    def btn_sqrt_action(self):
+    def btn_sqrt_action(tkinter):
 
-        previous_num = float(self.entry.get())
-        self.entry.delete(first=0, last=END)
+        previous_num = float(tkinter.entry.get())
+        tkinter.entry.delete(first=0, last=END)
         result = sqrt(previous_num)
         if str(result)[-1] == '0':
-            self.entry.insert(0, str(result)[:-2])
+            tkinter.entry.insert(0, str(result)[:-2])
         else:
-            self.entry.insert(0, str(result))
+            tkinter.entry.insert(0, str(result))
 
-    def btn_clear_action(self):
+    def btn_clear_action(self, tkinter):
 
-        self.entry.delete(first=0, last=END)
-        self.entry.insert(0, '0')
+        tkinter.entry.delete(first=0, last=END)
+        tkinter.entry.insert(0, '0')
 
         self.marker_set = None
 
 
-class Equal_action(Operations_main):
-    def btn_equal_action(self):
-        # print(marker_set)
-        # print(self.previous_res)
-        
+    def btn_equal_action(self, tkinter):
+         
+        self.counter_operations = 0
 
-        secondary_res = self.entry.get()
-        print(secondary_res)
+        secondary_res = tkinter.entry.get()
 
         if self.marker_set == 'plus':
-            result = round(float(self.previous_res) + float(secondary_res), 10)
-            self.entry.delete(first=0, last=END)
+            result = round(float(tkinter.previous_res) + float(secondary_res), 10)
+            tkinter.entry.delete(first=0, last=END)
 
             if str(result)[-1] == '0':
-                self.entry.insert(0, str(result)[:-2])
+                tkinter.entry.insert(0, str(result)[:-2])
             else:
-                self.entry.insert(0, str(result))
+                tkinter.entry.insert(0, str(result))
                 
         elif self.marker_set == 'minus':
-            result = round(float(self.previous_res) - float(secondary_res), 10)
-            self.entry.delete(first=0, last=END)
+            result = round(float(tkinter.previous_res) - float(secondary_res), 10)
+            tkinter.entry.delete(first=0, last=END)
             if str(result)[-1] == '0':
-                self.entry.insert(0, str(result)[:-2])
+                tkinter.entry.insert(0, str(result)[:-2])
             else:
-                self.entry.insert(0, str(result))
+                tkinter.entry.insert(0, str(result))
 
         elif self.marker_set == 'multiply':
-            result = round(float(self.previous_res) * float(secondary_res), 10)
-            self.entry.delete(first=0, last=END)
+            result = round(float(tkinter.previous_res) * float(secondary_res), 10)
+            tkinter.entry.delete(first=0, last=END)
             if str(result)[-1] == '0':
-                self.entry.insert(0, str(result)[:-2])
+                tkinter.entry.insert(0, str(result)[:-2])
             else:
-                self.entry.insert(0, str(result))
+                tkinter.entry.insert(0, str(result))
 
         elif self.marker_set == 'division':
             try:
-                self.entry.delete(first=0, last=END)
-                result = round(float(self.previous_res) / float(secondary_res), 10)
+                tkinter.entry.delete(first=0, last=END)
+                result = round(float(tkinter.previous_res) / float(secondary_res), 10)
 
                 if str(result)[-1] == '0':
-                    self.entry.insert(0, str(result)[:-2])
+                    tkinter.entry.insert(0, str(result)[:-2])
                 else:
-                    self.entry.insert(0, str(result))
+                    tkinter.entry.insert(0, str(result))
             except ZeroDivisionError:
-                self.entry.insert(0, '∞')
+                tkinter.entry.insert(0, '∞')
 
         else:
             try:
                 pass
             except: NameError
 
-# class B(Buttonsaction):
-#     def func(self):
-#         if marker_set != None:
-#             Buttonsaction.btn_equal_action()
+        # self.marker_set = False
+
+
+    def equal_action(self, tkinter):
+        
+        secondary_res = tkinter.entry.get()
+
+        if self.previous_operation == 'plus':
+            result = round(float(tkinter.previous_res) + float(secondary_res), 10)
+            tkinter.entry.delete(first=0, last=END)
+
+            if str(result)[-1] == '0':
+                tkinter.entry.insert(0, str(result)[:-2])
+            else:
+                tkinter.entry.insert(0, str(result))
+                
+        elif self.previous_operation == 'minus':
+            result = round(float(tkinter.previous_res) - float(secondary_res), 10)
+            tkinter.entry.delete(first=0, last=END)
+            if str(result)[-1] == '0':
+                tkinter.entry.insert(0, str(result)[:-2])
+            else:
+                tkinter.entry.insert(0, str(result))
+
+        elif self.previous_operation == 'multiply':
+            result = round(float(tkinter.previous_res) * float(secondary_res), 10)
+            tkinter.entry.delete(first=0, last=END)
+            if str(result)[-1] == '0':
+                tkinter.entry.insert(0, str(result)[:-2])
+            else:
+                tkinter.entry.insert(0, str(result))
+
+        elif self.previous_operation == 'division':
+            try:
+                tkinter.entry.delete(first=0, last=END)
+                result = round(float(tkinter.previous_res) / float(secondary_res), 10)
+
+                if str(result)[-1] == '0':
+                    tkinter.entry.insert(0, str(result)[:-2])
+                else:
+                    tkinter.entry.insert(0, str(result))
+            except ZeroDivisionError:
+                tkinter.entry.insert(0, '∞')
+
+        else:
+            try:
+                pass
+            except: NameError
